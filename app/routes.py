2995f87a2c2ad99597e3fdebf7c4e87460158c76
shamaiem10 <shamaiemshabbir14@gmail.com>
2025-07-15 18:57:13 +0500

    Initial commit with user-side feed design and dashboard UI

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session
+
 
 main = Blueprint('main', __name__)
+auth = Blueprint('auth', __name__)
 
 @main.route('/')
 def home():
@@ -9,3 +11,12 @@ def home():
 @main.route("/signup")
 def signup():
     return render_template("signup.html")
+
+@main.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@auth.route('/logout')
+def logout():
+    session.clear()  # Optional: clear session/login data
+    return redirect(url_for('main.home'))  # Redirects to "/"
